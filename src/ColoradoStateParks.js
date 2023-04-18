e040e22997fcb36eb3e95502ac91a61997cdc79a
kevin-wairi <kevinwairi@gmail.com>
2023-04-18 23:38:50 +0300

    Completed assignment

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,15 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import { trees, wildlife } from './parks/RockyMountain'
+import MesaVerde from './parks/MesaVerde'
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
+  console.log(trees);
+  wildlife();
+  MesaVerde();
 
   return <h1>Colorado State Parks!</h1>;
 }
+
+export default ColoradoStateParks
